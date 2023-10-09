5b9666272295ff23764b34da4a51ae05388f27c9
Oren <134296255+orenfackrell@users.noreply.github.com>
2023-10-09 21:53:41 +0100

    outline functions to replace checking functions

--- src/buttonFunc.js
+++ src/buttonFunc.js
@@ -13,23 +13,53 @@ function itsAllHookedUp() {
 
 function menuFunctions() {
   remove.addEventListener('click', () => {
-    itsAllHookedUp();
+    removeCurrentItem();
   });
   calendar.addEventListener('click', () => {
-    itsAllHookedUp();
+    openCalendar();
   });
   lightDark.addEventListener('click', () => {
-    itsAllHookedUp();
+    toggleTheme();
   });
   changePalette.addEventListener('click', () => {
-    itsAllHookedUp();
+    changeColours();
   });
   addItem.addEventListener('click', () => {
-    itsAllHookedUp();
+    createItem();
   });
   addProject.addEventListener('click', () => {
-    itsAllHookedUp();
+    createProject();
   });
+}
+
+function menuPopUp() {
+  // create a template popup which takes x amount of args
+  // use this in the other button functions
+}
+
+function removeCurrentItem() {
+  // add a way for the currently open todo item to be recorded, then simply remove the active one
+}
+
+function openCalendar() {
+  // open up a calendar which shows all of the todo items on their due date with their urgency shown
+}
+
+function toggleTheme() {
+  // invert light / dark themes
+}
+
+function changeColours() {
+  // give a menu for the user to give main + two accent colours to replace those on the page
+}
+
+function createItem() {
+  // create menu pop-up for the user to input the needed details for a new item
+  // have the default project being the one the user is currently in
+}
+
+function createProject() {
+  // create menu pop-up for the user to input the needed details for a new project
 }
 
 export default menuFunctions;
