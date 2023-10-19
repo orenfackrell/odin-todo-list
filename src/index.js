279994287b239461addc256cd8550273c1ce44a9
Oren <134296255+orenfackrell@users.noreply.github.com>
2023-10-19 16:05:45 +0100

    create better dummies for testing

--- src/index.js
+++ src/index.js
@@ -4,18 +4,26 @@ import { TodoItem } from './todoItem';
 import menuFunctions from './buttonFunc';
 
 // Create dummy projects
-const project1 = new Project('Dummy Project', 'Placeholder description', '20 - 10 - 2023');
-const todoItem1 = new TodoItem(
-  'Todo Item 1',
-  'Placeholder description',
-  '20 - 10 - 2023',
-  '20 - 10 - 2023',
-  '70%',
-  'some notes and shit',
-  '',
-);
-project1.addTodoItem(todoItem1);
-allProjects.push(project1);
+for (let i = 0; i < 6; i += 1) {
+  const newProject = new Project(`Dummy Project ${i}`, 'Placeholder description', '20 - 10 - 2023');
+  for (let j = 0; j < 4; j += 1) {
+    const todoItem = new TodoItem(
+      `Todo Item ${j}`,
+      'Placeholder description',
+      '2 - 10 - 2023',
+      `${j}*4 - 10 - 2023`,
+      '70%',
+      'some notes and shit',
+      '',
+    );
+    newProject.addTodoItem(todoItem);
+  }
+  allProjects.push(newProject);
+
+  if (i === 6) {
+    newProject.selected = true;
+  }
+}
 
 document.addEventListener('DOMContentLoaded', () => {
   loadProjectsFromLocalStorage();
